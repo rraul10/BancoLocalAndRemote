77c04ee23bff84c287a82223251ad40e4d39de2d
Yahya el Hadri el Bakkali <146001467+13elhadri@users.noreply.github.com>
2024-10-17 14:14:15 +0200

    Terminado el app module el app component y el retrofit de usuario. En el main esta llamando al servicio utiliznado dagger

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+
+import org.example.client.service.ClienteService;
+import org.example.client.service.ClienteServiceImpl;
+import org.example.di.AppComponent;
+import org.example.di.DaggerAppComponent;
+
 public class Main {
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ClienteService clienteService = DaggerAppComponent.create().clienteService();
     }
 }
