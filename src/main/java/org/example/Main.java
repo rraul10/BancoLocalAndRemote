fc5ae5891971096e76799535fcabc4fdac9b7b8f
Javier Hernández vicente <javier.hernandez@alumno.iesluisvives.org>
2024-10-21 01:02:38 +0200

    Resolving conflicts on csv and BBDD

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,36 @@
 package org.example;
 
-
-
-import org.example.di.AppComponent;
 import org.example.di.DaggerAppComponent;
 import org.example.service.ClienteService;
 
+import java.util.concurrent.CompletableFuture;
+
 public class Main {
-
-
 
     public static void main(String[] args) {
 
         ClienteService clienteService = DaggerAppComponent.create().clienteService();
+
+        CompletableFuture.runAsync(() -> {
+            while (true) {
+                try {
+                    clienteService.loadData();  // Ejecutar la función
+                    Thread.sleep(30000);  // Esperar 30 segundos
+                } catch (InterruptedException e) {
+                    System.out.println("La tarea fue interrumpida.");
+                    break;
+                }
+            }
+        });
+
+        CompletableFuture.runAsync(()->{
+                    clienteService.getAllClientes(false);
+                    clienteService.getAllUsers(true).forEach(i->
+                            i.forEach(System.out::println)
+                    );
+                }
+
+        );
+
     }
 }
