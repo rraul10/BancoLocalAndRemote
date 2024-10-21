42f31e96c593640dbf399a4a7fc6371273990f0c
Javier Ruiz <javier.ruiz.serrano59@gmail.com>
2024-10-21 11:27:30 +0200

    Last commit

--- src/main/java/org/example/storages/validators/CsvValidator.java
+++ src/main/java/org/example/storages/validators/CsvValidator.java
@@ -1,17 +1,13 @@
 package org.example.storages.validators;
 
 
-import org.example.models.TarjetaCredito;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.List;
 
-public class csvValidator {
-    private static final Logger logger = LoggerFactory.getLogger(csvValidator.class);
+public class CsvValidator {
+    private static final Logger logger = LoggerFactory.getLogger(CsvValidator.class);
     public static boolean csvValidatorImport(File file){
 
 
